ad6cdb1921636eda772509bc39267c9183809f60
LAPTOP-E2U5TP4H\ASUS <gilbranbambimboo@gmail.com>
2024-06-02 11:37:58 +0700

    view kategori

--- resources/views/frontend/kategori.blade.php
+++ resources/views/frontend/kategori.blade.php
@@ -38,7 +38,8 @@
     <div class="container">
         <h1 class="category-title text-center">Kategori produk UKM : {{ $kategori }}</h1>
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-            @foreach ($ukm as $ukms)
+            @if ($ukm->count() > 0)
+                        @foreach($ukm as $ukms)
                 <div class="col">
                     <div class="card h-100">
                         <img src="{{ asset('images/ukm/'. $ukms->foto) }}" alt="{{ $ukms->nama }}" class="card-img-top">
@@ -49,6 +50,17 @@
                     </div>
                 </div>
             @endforeach
+            @else
+            <div class="d-flex justify-content-center align-items-center" style="height: 80vh; text-align: center">
+                <div class="alert alert-info text-center p-4">
+                    <div class="display-4">
+                        <i class="fas fa-box-open"></i>
+                    </div>
+                    <h4 class="alert-heading mt-3">BELUM ADA PRODUK DENGAN KATEGORI INI</h4>
+                    <p>Silakan lihat kategori lainnya atau kembali nanti untuk pembaruan terbaru.</p>
+                </div>
+            </div>
+            @endif
         </div>
     </div>
 
